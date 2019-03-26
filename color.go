6740bb86d859cae74c0c9782af0465c57cb80536
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-26 09:36:27 +0900

    color: add Reset method

--- color.go
+++ color.go
@@ -191,6 +191,11 @@ func (c *Color) Prepend(value Attribute) {
 	c.params[0] = value
 }
 
+// Reset resets the c.params slice.
+func (c *Color) Reset() {
+	c.params = c.params[:0]
+}
+
 // Fprint formats using the default formats for its operands and writes to w.
 // Spaces are added between operands when neither is a string.
 // It returns the number of bytes written and any write error encountered.
