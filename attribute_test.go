1273a41b854b6853c43840fa7b2a274fde510b88
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-31 19:15:18 +0900

    attribute: add TestAttributeName testcases

--- attribute_test.go
+++ attribute_test.go
@@ -200,3 +200,195 @@ func TestAttributeString(t *testing.T) {
 		})
 	}
 }
+
+
+func TestAttributeName(t *testing.T) {
+	tests := []struct {
+		attr color.Attribute
+		want string
+	}{
+		{
+			attr: color.Reset,
+			want: "Reset",
+		},
+		{
+			attr: color.Bold,
+			want: "Bold",
+		},
+		{
+			attr: color.Faint,
+			want: "Faint",
+		},
+		{
+			attr: color.Italic,
+			want: "Italic",
+		},
+		{
+			attr: color.Underline,
+			want: "Underline",
+		},
+		{
+			attr: color.BlinkSlow,
+			want: "BlinkSlow",
+		},
+		{
+			attr: color.BlinkRapid,
+			want: "BlinkRapid",
+		},
+		{
+			attr: color.ReverseVideo,
+			want: "ReverseVideo",
+		},
+		{
+			attr: color.Concealed,
+			want: "Concealed",
+		},
+		{
+			attr: color.CrossedOut,
+			want: "CrossedOut",
+		},
+		{
+			attr: color.FgBlack,
+			want: "FgBlack",
+		},
+		{
+			attr: color.FgRed,
+			want: "FgRed",
+		},
+		{
+			attr: color.FgGreen,
+			want: "FgGreen",
+		},
+		{
+			attr: color.FgYellow,
+			want: "FgYellow",
+		},
+		{
+			attr: color.FgBlue,
+			want: "FgBlue",
+		},
+		{
+			attr: color.FgMagenta,
+			want: "FgMagenta",
+		},
+		{
+			attr: color.FgCyan,
+			want: "FgCyan",
+		},
+		{
+			attr: color.FgWhite,
+			want: "FgWhite",
+		},
+		{
+			attr: color.FgHiBlack,
+			want: "FgHiBlack",
+		},
+		{
+			attr: color.FgHiRed,
+			want: "FgHiRed",
+		},
+		{
+			attr: color.FgHiGreen,
+			want: "FgHiGreen",
+		},
+		{
+			attr: color.FgHiYellow,
+			want: "FgHiYellow",
+		},
+		{
+			attr: color.FgHiBlue,
+			want: "FgHiBlue",
+		},
+		{
+			attr: color.FgHiMagenta,
+			want: "FgHiMagenta",
+		},
+		{
+			attr: color.FgHiCyan,
+			want: "FgHiCyan",
+		},
+		{
+			attr: color.FgHiWhite,
+			want: "FgHiWhite",
+		},
+		{
+			attr: color.BgBlack,
+			want: "BgBlack",
+		},
+		{
+			attr: color.BgRed,
+			want: "BgRed",
+		},
+		{
+			attr: color.BgGreen,
+			want: "BgGreen",
+		},
+		{
+			attr: color.BgYellow,
+			want: "BgYellow",
+		},
+		{
+			attr: color.BgBlue,
+			want: "BgBlue",
+		},
+		{
+			attr: color.BgMagenta,
+			want: "BgMagenta",
+		},
+		{
+			attr: color.BgCyan,
+			want: "BgCyan",
+		},
+		{
+			attr: color.BgWhite,
+			want: "BgWhite",
+		},
+		{
+			attr: color.BgHiBlack,
+			want: "BgHiBlack",
+		},
+		{
+			attr: color.BgHiRed,
+			want: "BgHiRed",
+		},
+		{
+			attr: color.BgHiGreen,
+			want: "BgHiGreen",
+		},
+		{
+			attr: color.BgHiYellow,
+			want: "BgHiYellow",
+		},
+		{
+			attr: color.BgHiBlue,
+			want: "BgHiBlue",
+		},
+		{
+			attr: color.BgHiMagenta,
+			want: "BgHiMagenta",
+		},
+		{
+			attr: color.BgHiCyan,
+			want: "BgHiCyan",
+		},
+		{
+			attr: color.BgHiWhite,
+			want: "BgHiWhite",
+		},
+		{
+			attr: color.Attribute(200),
+			want: "Attribute(200)",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.want, func(t *testing.T) {
+			t.Parallel()
+
+			if got := tt.attr.Name(); got != tt.want {
+				t.Errorf("got %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
