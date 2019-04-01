98850d849e0c1cc63750aeb198d85c86003d18d7
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-01 13:10:22 +0900

    benchmarks: remove doc.go

--- benchmarks/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 The color Authors. All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-// +build benchmark benchmark_fatih
-
-package benchmarks
