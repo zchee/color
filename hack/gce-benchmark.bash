a29aa5b34f703b803b4c7ec13bd8dc2907fd48ca
Koichi Shiraishi <zchee.io@gmail.com>
2019-05-06 10:38:49 +0900

    hack: more useful gce-benchmark.bash script

--- hack/gce-benchmark.bash
+++ hack/gce-benchmark.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# run GCE instance command
+#  $ GOLANG_VERSION='1.12.4 or whatever' GOOGLE_CLOUD_PROJECT='foo' MACHINE_TYPE='n1-standard-{n}' GSUTIL_BENCHSTAT_BUCKET_NAME='gs://foo/benchstat/'; gcloud --project="$GOOGLE_CLOUD_PROJECT" alpha compute instances create --zone 'asia-northeast1-a' --machine-type "$MACHINE_TYPE" --image-project='debian-cloud' --image-family='debian-9' --boot-disk-type='pd-ssd' --preemptible --scopes 'https://www.googleapis.com/auth/cloud-platform' --metadata="golang_version=${GOLANG_VERSION},gsutil_benchstat_bucket_name=${GSUTIL_BENCHSTAT_BUCKET_NAME}" --metadata-from-file='startup-script=hack/gce-benchmark.bash' --async --verbosity='debug' 'benchstat'
+
+set -x
+
 start_time="$(date -u +%s)"
 
 echo 'Install google-fluentd ...'
@@ -12,7 +17,7 @@ service google-fluentd restart
 # Start tasks
 
 sudo apt-get update
-sudo apt-get install -yqq --no-install-recommends \
+sudo apt-get install -yqq --no-install-recommends --no-install-suggests \
   ca-certificates \
   curl \
   netbase \
@@ -32,16 +37,19 @@ sudo apt-get install -yqq --no-install-recommends \
   make \
   pkg-config
 
-export GOLANG_VERSION=1.12.1
+PACKAGE='github.com/zchee/color'
+
+GOLANG_VERSION="$(curl -s -H 'Metadata-Flavor:Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/golang_version)"
 
 wget -qq -O ~/go.tgz "https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz"
 sudo tar -C /usr/local -xzf ~/go.tgz
 rm ~/go.tgz
 
 export GOPATH=~/go
-export GOCACHE=~/go-build
+export GOCACHE=~/.cache/go-build
 export PATH="$GOPATH/bin:/usr/local/go/bin:$PATH"
 mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
+mkdir -p "$GOCACHE" && chmod -R 777 "$GOCACHE"
 
 go version
 
@@ -50,16 +58,31 @@ go get -u -v github.com/zchee/color
 
 export GO111MODULE=on
 
-cd "$GOPATH/src/github.com/zchee/color/benchmarks" || return
+cd "$GOPATH/src/$PACKAGE/benchmarks" || return
 go mod download
 go mod tidy -v
 go mod vendor -v
 
 REVISION="$(git rev-parse --short -q HEAD)"
-go test -v -mod=vendor -tags=benchmark -count 10 -run='^$' -bench=. | tee "new.$REVISION.txt" || true
-benchstat old.gce.golden.txt "new.$REVISION.txt" | tee "benchstat.$REVISION.txt" || true
+BENCH_OUT_BASE="$PWD/$(echo "$PACKAGE" | tr '/' '-')@$REVISION"
+NUM_CPU="$(nproc)"
 
-gsutil cp "old.$REVISION.txt" "new.$REVISION.txt" "benchstat.$REVISION.txt" "$(curl -s -H 'Metadata-Flavor:Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/gsutil_benchstat_bucket_name)" || true
+CPU_FLAG="$((NUM_CPU/4)),$((NUM_CPU/2)),$NUM_CPU"
+
+#  Clear PageCache, dentries and inodes
+sync; echo 3 > /proc/sys/vm/drop_caches
+go test -v -mod=vendor -tags=benchmark -cpu "$CPU_FLAG" -run='^$' -count 8 -bench=. -benchtime=10s . -fatih | tee "${BENCH_OUT_BASE}.old.txt" 2>&1 | go tool test2json
+
+#  Clear PageCache, dentries and inodes
+sync; echo 3 > /proc/sys/vm/drop_caches
+go test -v -mod=vendor -tags=benchmark -cpu "$CPU_FLAG" -run='^$' -count 8 -bench=. -benchtime=10s . | tee "${BENCH_OUT_BASE}.new.txt" 2>&1 | go tool test2json
+
+benchstat "${BENCH_OUT_BASE}.old.txt" "${BENCH_OUT_BASE}.new.txt" | tee "${BENCH_OUT_BASE}.benchstat.txt"
+
+CPUINFO_OUT="${BENCH_OUT_BASE}.cpuinfo.txt"
+cat /proc/cpuinfo > "$CPUINFO_OUT"
+
+gsutil cp "${BENCH_OUT_BASE}.old.txt" "${BENCH_OUT_BASE}.new.txt" "${BENCH_OUT_BASE}.benchstat.txt" "$CPUINFO_OUT" "$(curl -s -H 'Metadata-Flavor:Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/gsutil_benchstat_bucket_name)" || true
 
 # End tasks
 # ----------------------------------------------------------------------------
